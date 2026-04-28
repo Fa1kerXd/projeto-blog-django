45d8cba14de0ab99aed26efb9966e9632229680a
Augusto Cesar <fa1ker@icloud.com>
2026-04-28 18:43:54 -0400

    Project Blog for portfolio

---
+++ djangoapp/blog/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator
+from blog.models import Post
+
+# Create your views here.
+
+
+
+PER_PAGE = 9
+
+
+def index(request):
+    posts = Post.objects.get_published() #type:ignore
+    paginator = Paginator(posts,PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": page_obj,
+        }
+    )
+
+def created_by(request, author_pk):
+    posts = Post.objects.get_published().filter(created_by__pk=author_pk) #type:ignore
+    paginator = Paginator(posts,PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": page_obj,
+        }
+    )
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug) #type:ignore
+    paginator = Paginator(posts,PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+
+    return render(
+        request,
+        "blog/pages/index.html",
+        {
+            "page_obj": page_obj,
+        }
+    )
+
+
+def page(request):
+
+    return render(
+        request,
+        "blog/pages/page.html",
+        {
+            # "page_obj": page_obj
+        }
+    )
+
+def post(request, slug):
+    post = (Post.objects.get_published().filter(slug=slug).first()) #type:ignore
+
+    return render(
+        request,
+        "blog/pages/post.html",
+        {
+            "post": post
+        }
+    )
